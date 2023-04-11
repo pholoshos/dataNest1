cd09456ee0246cee83fe4c14475a738b6da2665c
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-04-11 21:14:38 +0200

    add actions

---
+++ core/actions/create.js
@@ -0,0 +1,7 @@
+const createEntity = () => {};
+
+const createApi = () => {};
+
+const createTable = () => {};
+
+module.exports = { createEntity, createApi, createTable };
---
+++ core/actions/get.js
@@ -0,0 +1,13 @@
+const getProperty = () => {
+  return {};
+};
+
+const getValue = () => {
+  return "value";
+};
+
+const findValue = () => {
+  return "value";
+};
+
+module.exports = { getProperty, getValue, findValue };
