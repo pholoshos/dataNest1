815a0617af6045e3b321fda325c91d46c0746ff3
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-04-11 21:05:55 +0200

    data

--- Schema/user.js
+++ Schema/user.js
@@ -1,22 +1,21 @@
 // Require Mongoose
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define a schema
 const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   authtoken: String,
-  createdDate :  {
+  createdDate: {
     type: Date,
     required: true,
-    default: Date.now // optional default value
+    default: Date.now, // optional default value
   },
-  companyName : String
-
+  companyName: String,
 });
 
 // Create a model from the schema
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 // Export the model
 module.exports = User;
