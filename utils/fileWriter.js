e7ee2d827828acf6fcbd85ed356c6881f20f4e5a
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-04-11 21:07:07 +0200

    data

--- utils/fileWriter.js
+++ utils/fileWriter.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
-const path = ''
+const fs = require("fs");
+const path = "";
 
-const fileWriter = (path,data)=>{
-  fs.writeFile(path, JSON.stringify({data}), (err) => {
+const fileWriter = (path, data) => {
+  fs.writeFile(path, JSON.stringify({ data }), (err) => {
     if (err) throw err;
-    console.log('endpoints written to file',data);
-  });  
-}
+    console.log("endpoints written to file", data);
+  });
+};
 
 module.exports = fileWriter;
