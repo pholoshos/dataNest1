a27f4706c9c5f56d42200e6f33f9ee5383149d66
Pholosho Seloane <35956639+pholoshos@users.noreply.github.com>
2023-04-20 09:02:11 +0200

    clean up

---
+++ datanestML/app/apiGenerator/main.py
@@ -0,0 +1,44 @@
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def create_api(command):
+    entities = {}
+    doc = nlp(command)
+    for ent in doc.ents:
+        entities[ent.label_] = ent.text
+
+    endpoint = entities.get("endpoint")
+    if not endpoint:
+        return "Please provide an endpoint."
+
+    method = entities.get("method", "GET")
+
+    handler = ""
+    if method == "GET":
+        if "id" in entities:
+            handler = (
+                f"(req, res) => {{res.send(find('{endpoint}', {{id: req.params.id}}))}}"
+            )
+        else:
+            handler = f"(req, res) => {{res.send(getAll('{endpoint}'))}}"
+    elif method == "POST":
+        handler = f"(req, res) => {{add('{endpoint}', req.body); res.send('{endpoint.capitalize()} created')}}"
+    elif method == "PUT":
+        handler = f"(req, res) => {{let {endpoint} = find('{endpoint}', {{id: req.params.id}}); if ({endpoint}) {{{endpoint}.name = req.body.name; {endpoint}.email = req.body.email; res.send('{endpoint.capitalize()} updated')}} else {{res.send('{endpoint.capitalize()} not found')}}}}"
+    elif method == "DELETE":
+        handler = f"(req, res) => {{let {endpoint} = find('{endpoint}', {{id: req.params.id}}); if ({endpoint}) {{delete('{endpoint}', {endpoint}); res.send('{endpoint.capitalize()} deleted')}} else {{res.send('{endpoint.capitalize()} not found')}}}}"
+    else:
+        return "Invalid method provided."
+
+    return {
+        "endpoints": [
+            {
+                "path": "/",
+                "method": "GET",
+                "handler": "(req, res) => {res.send('Hello World!')}",
+            },
+            {"path": f"/{endpoint}", "method": method, "handler": handler},
+        ]
+    }
--- datanestML/run.py
+++ datanestML/run.py
@@ -1,44 +1,10 @@
-import spacy
+from flask import Flask
 
-nlp = spacy.load("en_core_web_sm")
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
-def create_api(command):
-    entities = {}
-    doc = nlp(command)
-    for ent in doc.ents:
-        entities[ent.label_] = ent.text
-
-    endpoint = entities.get("endpoint")
-    if not endpoint:
-        return "Please provide an endpoint."
-
-    method = entities.get("method", "GET")
-
-    handler = ""
-    if method == "GET":
-        if "id" in entities:
-            handler = (
-                f"(req, res) => {{res.send(find('{endpoint}', {{id: req.params.id}}))}}"
-            )
-        else:
-            handler = f"(req, res) => {{res.send(getAll('{endpoint}'))}}"
-    elif method == "POST":
-        handler = f"(req, res) => {{add('{endpoint}', req.body); res.send('{endpoint.capitalize()} created')}}"
-    elif method == "PUT":
-        handler = f"(req, res) => {{let {endpoint} = find('{endpoint}', {{id: req.params.id}}); if ({endpoint}) {{{endpoint}.name = req.body.name; {endpoint}.email = req.body.email; res.send('{endpoint.capitalize()} updated')}} else {{res.send('{endpoint.capitalize()} not found')}}}}"
-    elif method == "DELETE":
-        handler = f"(req, res) => {{let {endpoint} = find('{endpoint}', {{id: req.params.id}}); if ({endpoint}) {{delete('{endpoint}', {endpoint}); res.send('{endpoint.capitalize()} deleted')}} else {{res.send('{endpoint.capitalize()} not found')}}}}"
-    else:
-        return "Invalid method provided."
-
-    return {
-        "endpoints": [
-            {
-                "path": "/",
-                "method": "GET",
-                "handler": "(req, res) => {res.send('Hello World!')}",
-            },
-            {"path": f"/{endpoint}", "method": method, "handler": handler},
-        ]
-    }
+if __name__ == '__main__':
+    app.run(debug=True)
